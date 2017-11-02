d6621c0532604087c414c96c25fcd84c24d36eb3
Sean Hunt <srhunt@wpi.edu>
2017-11-01 20:08:09 -0400

    Fixed Ian's System print

--- teamlist.java
+++ teamlist.java
@@ -2,6 +2,6 @@ public class teamlist{
   public static void main(String[] args){
     System.out.println("Zach Halzel");
     System.out.println("Sean Hunt");
-    system.out.println("Ian");
+    System.out.println("Ian");
   }
 }
