031c22ab499ec4302ebe416bedec318e7618ef66
alexander <apappas@wpi.edu>
2017-11-01 20:13:37 -0400

    Pappas Test

--- teamlist.java
+++ teamlist.java
@@ -2,6 +2,7 @@ public class teamlist{
   public static void main(String[] args){
     System.out.println("Zach Halzel");
     system.out.println("Melanie says hello");
+    System.out.println("Pappas is back");
     system.out.println("Ian");
     System.out.println("Lucas Sacherer");
   }
